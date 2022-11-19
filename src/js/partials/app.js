12c5745368f129e094714261117c40e2858af6c3
Nikolai <k_k_007@mail.ru>
2022-11-20 00:05:24 +0600

    feat: add Рекламный каталог page

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -495,6 +495,48 @@ var uikit = {
       }
     });
 
+    if ($('.js-slider-product').length) {
+      $('.js-slider-product').each(function () {
+        var $this = $(this);
+
+        $this.slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          variableWidth: false,
+          arrows: true,
+          dots: false,
+          infinite: true,
+          adaptiveHeight: true,
+          autoplay: false,
+          autoplaySpeed: 2000,
+          lazyLoad: 'progressive',
+          prevArrow: '<a href="#" class="product-block__prev slick-arrow slick-prev" tabindex="-1"><svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_2894_13921)"><rect width="48" height="49" rx="24" fill="white" fill-opacity="0.2"/><path d="M4.90909 8.90909L30 24.5L4.90909 40.0909L-20.1818 24.5L4.90909 8.90909Z" fill="#E40032"/></g><defs><clipPath id="clip0_2894_13921"><rect width="48" height="49" rx="24" fill="white"/></clipPath></defs></svg></a>',
+          nextArrow: '<a href="#" class="product-block__next slick-arrow slick-next" tabindex="-1"><svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_2894_13921)"><rect width="48" height="49" rx="24" fill="white" fill-opacity="0.2"/><path d="M4.90909 8.90909L30 24.5L4.90909 40.0909L-20.1818 24.5L4.90909 8.90909Z" fill="#E40032"/></g><defs><clipPath id="clip0_2894_13921"><rect width="48" height="49" rx="24" fill="white"/></clipPath></defs></svg></a>',
+          responsive: [{
+            breakpoint: xs,
+            settings: {
+              arrows: false,
+              dots: true,
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }]
+        });
+
+      });
+
+      $('.js-team-slider-prev').click(function () {
+        $('.js-team-slider').slick('slickPrev');
+        return false;
+      });
+      $('.js-team-slider-next').click(function () {
+        $('.js-team-slider').slick('slickNext');
+        return false;
+      });
+
+    }
+
     if ($('.js-slider-object').length) {
       $('.js-slider-object').each(function () {
         var $this = $(this);
